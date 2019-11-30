ba6e3ba9a5d65b94f0ee3ec4c501218c6d58ad27
Josh Mitchell <yoshanuikabundi@gmail.com>
2019-12-01 00:18:34 +1100

    more tests

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,7 +6,7 @@ use noether::io;
 use noether::lj_from_positions;
 
 fn main() {
-    let positions = io::read_positions("test_targets/equil.trr").unwrap();
+    let positions = io::read_positions("test_targets/2_atoms/2_atoms_frommax.trr").unwrap();
 
     let energies = lj_from_positions(
         positions,
@@ -15,14 +15,27 @@ fn main() {
         1.2 * NANOMETER
     );
 
-    let energies_ref = io::read_xvg("test_targets/equil.xvg", 1).unwrap();
+    let energies_ref = io::read_xvg("test_targets/2_atoms/2_atoms_frommax.xvg", 1).unwrap();
     for (a, b) in energies.iter().zip(energies_ref.iter()) {
         let b: f64 = b.parse().unwrap();
-        println!("diff = {:?}", *a - (b * KJPERMOL));
+        println!("noether = {:?}, gromacs = {:?}", *a, b * KJPERMOL);
     }
 
-    // for a in energies.iter() {
-    //     println!("{:?}", a);
-    // }
+
+    let positions = io::read_positions("test_targets/100_atoms/100_atoms.trr").unwrap();
+
+    let energies = lj_from_positions(
+        positions,
+        0.3405 * NANOMETER,
+        1.0000 * KJPERMOL,
+        1.2 * NANOMETER
+    );
+
+    let energies_ref = io::read_xvg("test_targets/100_atoms/100_atoms.xvg", 1).unwrap();
+    for (a, b) in energies.iter().zip(energies_ref.iter()) {
+        let b: f64 = b.parse().unwrap();
+        println!("noether = {:?}, gromacs = {:?}", *a, b * KJPERMOL);
+    }
+
 
 }
